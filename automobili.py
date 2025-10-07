ea13b58f8b987cc60ac3522e44c963cd43e07a16
andre-saccoo <andre.sacco@icloud.com>
2025-10-07 14:17:30 +0200

    salvataggio in laboratorio

---
+++ automobili.py
@@ -0,0 +1,11 @@
+class Automobile:
+    def __init__(self,id, marca, modello, anno, numPosti):
+        self.id = id
+        self.marca = marca
+        self.modello = modello
+        self.anno = anno
+        self.numPosti = numPosti
+
+    def __str__(self):
+        return f'identificatore macchina:{self.id},marca:{self.marca}, modello:{self.modello},anno: {self.anno},numero posti{self.numPosti}'
+
--- autonoleggio.py
+++ autonoleggio.py
@@ -1,15 +1,37 @@
+from automobili import Automobile
+
 class Autonoleggio:
-    def __init__(self, nome, responsabile):
+    def __init__(self, nome, responsabile,listaAutomobili):
         """Inizializza gli attributi e le strutture dati"""
-        # TODO
+        self._nome = nome
+        self._responsabile = responsabile
+        self.listaAutomobili = []
 
     def carica_file_automobili(self, file_path):
-        """Carica le auto dal file"""
-        # TODO
+        try:
+            infile=open(file_path, 'r')
+            for line in infile:
+                a=Automobile(line[0],line[1],line[2],line[3],line[4])
+                self.listaAutomobili.append(a)
+            infile.close()
+        except FileNotFoundError:
+            print("file non trovato")
+            return None
 
-    def aggiungi_automobile(self, marca, modello, anno, num_posti):
+    def aggiungi_automobile(self, marca, modello, anno, num_posti, ):
         """Aggiunge un'automobile nell'autonoleggio: aggiunge solo nel sistema e non aggiorna il file"""
         # TODO
+        id=f"A+{len(self.listaAutomobili)+1}"
+        marca=input("inserire la marca dell'automobile: ")
+        modello=input("inserire la modello dell'automobile: ")
+        try:
+            anno=input("inserire la anno dell'automobile: ")
+        except ValueError:
+            print("inserire la anno dell'automobile:")
+        try:
+            num_posti=int(input("inserire la numero posti: "))
+        except ValueError:
+            print("inserire la numero posti:")
 
     def automobili_ordinate_per_marca(self):
         """Ordina le automobili per marca in ordine alfabetico"""
